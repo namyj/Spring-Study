e06d433d0e022d1befd096df3d0099aebe814e30
namyj <umjiuji61@gmail.com>
2024-01-09 20:05:37 +0900

    feat: MemberServiceImple 클래스 생성

---
+++ spring-basic/src/main/java/com/example/basic/member/MemberServiceImple.java
@@ -0,0 +1,16 @@
+package com.example.basic.member;
+
+public class MemberServiceImple implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long id) {
+        return memberRepository.findById(id);
+    }
+}
