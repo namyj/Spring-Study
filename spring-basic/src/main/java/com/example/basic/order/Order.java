eafc8fc4ad886d1d4ff617b5195940b419a09b00
namyj <umjiuji61@gmail.com>
2024-01-14 19:42:07 +0900

    feat: Order class 생성

---
+++ spring-basic/src/main/java/com/example/basic/order/Order.java
@@ -0,0 +1,68 @@
+package com.example.basic.order;
+
+/**
+ * 주문서
+ */
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    /**
+     * @return Item 최종 가격
+     */
+    public int calculatePrice() {
+        return itemPrice - discountPrice > 0 ?  itemPrice - discountPrice : itemPrice;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
